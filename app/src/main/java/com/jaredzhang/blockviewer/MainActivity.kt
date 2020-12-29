7c8ddfc483f4c77345c966da9759ca0f48aa90ca
zhuodong <zhuodong.zhang@go-jek.com>
2020-12-29 20:47:45 +0800

    Add list UI

--- app/src/main/java/com/jaredzhang/blockviewer/MainActivity.kt
+++ app/src/main/java/com/jaredzhang/blockviewer/MainActivity.kt
@@ -2,9 +2,6 @@ package com.jaredzhang.blockviewer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 
 class MainActivity : AppCompatActivity() {
 
